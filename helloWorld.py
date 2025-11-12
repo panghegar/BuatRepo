7cdbeffc6ffcf39ab9216fc5944bbfa05427cdf4
rahmat <rahmathegar52@gmail.com>
2025-11-12 17:11:30 +0700

    feat: pengurangan

--- helloWorld.py
+++ helloWorld.py
@@ -2,6 +2,8 @@ print("Ini branch main")
 
 a = 10
 b = 20
-tambah = a + b  
-print("Hasil penjumlahan:", tambah)
 
+tambah = a + b
+pengurangan = b - a
+print("Hasil penjumlahan:", tambah)
+print("Hasil pengurangan:", pengurangan)
